d3e206be815ac1720709be37efb7f3a5a2294072
MartinBilson <martingmaina97@gmail.com>
2024-12-30 10:03:32 +0300

    uncommented code

--- main.py
+++ main.py
@@ -1,42 +1,42 @@
-# x = int(input("What is x? "))
-# y = int(input("What is y? "))
+x = int(input("What is x? "))
+y = int(input("What is y? "))
 
-# print(x+y)
-# x = float(input("Enter your number "))
-# y = float(input("Enter your number "))
+print(x+y)
+x = float(input("Enter your number "))
+y = float(input("Enter your number "))
 
-# z = round(x +y)
+z = round(x +y)
 
-# #A string formatter
-# print(f" {z:,}") 
+#A string formatter
+print(f" {z:,}") 
 
-# #TESTING STEP 2
-# #I want to keep the entire domain including the '.com'
-# x = ['www.a.com', 'www.b.com', 'www.c.com']
-# y = []
+#TESTING STEP 2
+#I want to keep the entire domain including the '.com'
+x = ['www.a.com', 'www.b.com', 'www.c.com']
+y = []
 
-# for i in x:
-#     y.append(i.split('.')[1] + '.' + i.split('.')[2])
-# print(y)
+for i in x:
+    y.append(i.split('.')[1] + '.' + i.split('.')[2])
+print(y)
   
-# #function
-# #Define a function hello, parameter hello and assign value
-# def hello(to = "World"):
-#     print("Hello ", to)
+#function
+#Define a function hello, parameter hello and assign value
+def hello(to = "World"):
+    print("Hello ", to)
 
-# #user to enter his name and call the function
-# name = input("What is your full names: ")
-# hello(name)
+#user to enter his name and call the function
+name = input("What is your full names: ")
+hello(name)
 
-# #def main func, variable x and ask input and return its square
-# def main():
-#     x = int(input("Enter the value of x: "))
-#     print("X squared is: ", square(x))
+#def main func, variable x and ask input and return its square
+def main():
+    x = int(input("Enter the value of x: "))
+    print("X squared is: ", square(x))
 
-# def square(n):
-#     return n **2
+def square(n):
+    return n **2
 
-# main()
+main()
 
 #Conditional statements (questionning styles))
 x = int(input("whats x value: "))
