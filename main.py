5f89584b3bedce67fe28801c88d6103b34547698
MartinBilson <martingmaina97@gmail.com>
2025-01-16 02:00:27 +0300

    modified functions

--- main.py
+++ main.py
@@ -19,7 +19,7 @@
 #     y.append(i.split('.')[1] + '.' + i.split('.')[2])
 # print(y)
   
-# #function
+# #FUNCTIONS
 # #Define a function hello, parameter hello and assign value
 # def hello(to = "World"):
 #     print("Hello ", to)
@@ -38,13 +38,59 @@
 
 # main()
 
-#Conditional statements (questionning styles))
-x = int(input("whats x value: "))
-y = int(input("Whats y value: "))
+# #CONDITIONAL STATEMENTS
+# #if statements
+# x = int(input("whats x value: "))
+# y = int(input("Whats y value: "))
 
-if x < y:
-    print(f"x value of {x} is less than y value of {y}")
-if x > y:
-    print(f"x value of {x} is greater than y value of {y}")
-if x == y:
-    print(f"x value of {x} is equal to y value of {y}")
+# if x < y:
+#     print(f"x value of {x} is less than y value of {y}")
+# if x > y:
+#     print(f"x value of {x} is greater than y value of {y}")
+# if x == y:
+#     print(f"x value of {x} is equal to y value of {y}")
+
+# #Conditional statements (else if)
+# #student marks
+# score = int(input("Whats your score?:"))
+
+# if score >= 90 and score <=100:
+#     print("Grade: A")
+# elif score >= 80 and score < 90:
+#     print("Grade: B")
+# elif score >= 70 and score < 80:
+#     print("Grade: C")
+# elif score >= 60 and score < 70:
+#     print("Grade: D")
+# else:
+#     print("Grade: F")
+
+# #Determine if the number is even and odd
+# def main():
+#     x = int(input("What is the value x: "))
+#     if is_even(x):
+#         print("Even")
+#     else:
+#         print("Odd")
+
+# def is_even(n):
+#     if n%2 == 0:
+#         return True
+#     else:
+#         return False
+        
+# main()
+
+# FUNCTIONS IN PYTHON that takes a number and return its square
+#Example 1 how to execute it
+def square(x):
+    return x * x
+
+for i in range(30):
+    print(f"The square of {i} is {square(i)}")  
+
+#Example 2 how to execute it
+from function import square
+
+for i in range(10):
+    print(f"The square if {i} is {square(i)}")
